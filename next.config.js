966085c0e522d102002fcb6d5badc42f391d4d60
Rikimbili <raciel2901@gmail.com>
2022-10-05 13:05:29 -0400

    fix(redirect): Specify in next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: "/resume",
+        destination:
+          "https://drive.google.com/file/d/1QeabHVP-wJERMum3EX4oRIN4x6WCOPXl/preview",
+        permanent: true,
+      },
+    ];
+  },
 };
