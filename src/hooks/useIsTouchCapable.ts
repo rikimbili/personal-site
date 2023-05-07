9ca0590f1d31ff36b0a4094838303ccbaace9f6f
Rikimbili <git@raciel.dev>
2023-05-07 14:22:43 -0400

    refactor(useIsTouchCapable): Update on viewport size change

--- src/hooks/useIsTouchCapable.ts
+++ src/hooks/useIsTouchCapable.ts
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
+import { useScreen } from "usehooks-ts";
 
 export default function useIsTouchCapable() {
   const [isTouchCapable, setIsTouchCapable] = useState(false);
+  const screen = useScreen();
 
   useEffect(() => {
     setIsTouchCapable("ontouchstart" in window || navigator.maxTouchPoints > 0);
-  }, []);
+  }, [screen]);
 
   return isTouchCapable;
 }
